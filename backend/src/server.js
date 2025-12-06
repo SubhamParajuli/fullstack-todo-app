27492338bf07cfb542bcb4694615b13e164cfc5f
Subham Parajuli <suvamparajuli222@gmail.com>
2025-12-06 12:01:00 +0545

    completed schema

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import { connectDB } from './config/db.js';
 import dotenv from"dotenv";
 dotenv.config()
 const app=express()
-const PORT=5001
+const PORT=process.env.PORT || 5001
 connectDB();
 app.use("/api/notes",notesRoutes);
 
